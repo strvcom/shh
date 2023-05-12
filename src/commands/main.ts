273aec0eb13609d15f16c4e24a338dc28f80bf4f
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2023-05-12 19:17:27 +0200

    feat: added environment selection

--- src/commands/main.ts
+++ src/commands/main.ts
@@ -1,20 +1,71 @@
 import { Command } from 'commander'
+import { globSync } from 'glob'
+import log from 'log-update'
+import inquirer from 'inquirer'
 
-import { getConfig } from '../lib/config'
+import { getConfig, SecrecyConfig } from '../lib/config'
+import path from 'path'
 
 interface Options {
   secret: string
   environment: string
 }
 
+/**
+ * Resolve environment paths and their names.
+ */
+const getEnvironments = (config: SecrecyConfig) => {
+  const pattern = config.environments.replace('[name]', '*')
+
+  const regex = new RegExp(
+    `^${path
+      .resolve(config.cwd, config.environments)
+      .replace('**', '.+')
+      .replace('*', '[^ /]+')
+      .replace('[name]', '(?<name>[a-zA-Z0-9]+)')}$`
+  )
+
+  const environments = globSync(pattern, { cwd: config.cwd, absolute: true }).map((file) => {
+    const name = file.match(regex)?.groups?.name
+
+    if (!name) {
+      throw new Error(`Could not resolve enviromnet name for file: "${file}"`)
+    }
+
+    return { file, name }
+  })
+
+  return environments
+}
+
+/**
+ * Inquire environment.
+ */
+const selectEnvironment = async (environments: ReturnType<typeof getEnvironments>) =>
+  (
+    await inquirer.prompt([
+      {
+        name: 'environment',
+        type: 'list',
+        message: 'Select the environment to install',
+        choices: environments.map(({ name }) => name),
+      },
+    ])
+  ).environment as string
+
 // Declare the command-line program.
 const command = new Command()
   .option('-s, --secret <path>', 'The path to the secret file')
   .option('-e, --environment <name>', 'The environment to install')
-  .action(function (this: Command, options: Options) {
+  .action(async (options: Options) => {
     const config = getConfig(options)
+    const environments = getEnvironments(config)
+    const selected = config.environment ?? (await selectEnvironment(environments))
+    const environment = environments.find(({ name }) => name === selected)
 
-    console.log({ config })
+    if (!environment) {
+      throw new Error(`File inexistant for environment "${selected}".`)
+    }
   })
 
 export { command }
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -11,22 +11,28 @@ interface SecrecyConfig {
    * Path pattern to environment files.
    */
   environments: string
+
+  /**
+   * Resolved project root.
+   */
+  cwd: string
 }
 
 const defaults = {
   secret: './env/secret',
-  environments: './env/.env.[name].encrypted',
+  environments: './env/.env.encrypted.[name]',
 }
 
 const getConfig = <Override extends {}>(override: Override) => {
   const root = process.cwd()
   const configPath = path.resolve(root, '.secrecyrc')
 
-  const config: Partial<SecrecyConfig> = fs.existsSync(configPath)
+  const config: Omit<Partial<SecrecyConfig>, 'root'> = fs.existsSync(configPath)
     ? JSON.parse(fs.readFileSync(configPath, 'utf-8'))
     : {}
 
-  return { ...defaults, ...config, ...override }
+  return { ...defaults, ...config, ...override, cwd: root }
 }
 
 export { getConfig }
+export type { SecrecyConfig }
