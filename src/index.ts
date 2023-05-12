e131aa17c562829c5862bc841f07f9c7e2908409
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2023-05-12 17:05:51 +0200

    feat: initiated cli tool

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("hello world");
+import { Command } from 'commander'
+// @ts-ignore
+import pkg from '../package.json'
+
+// Declare the command-line program.
+const program = new Command().name(pkg.name).description(pkg.description).version(pkg.version)
+
+// Execute.
+program.parse(process.argv)
