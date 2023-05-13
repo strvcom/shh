015b4241c6c9a80af68fbd45271e33ced0ddc810
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2023-05-13 20:44:15 +0200

    feat: reduce ux sleeping time

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,7 +3,7 @@ import logUpdate from 'log-update'
 /**
  * Forcefully sleep execution (for better UX).
  */
-const sleep = (ms = 500) => new Promise((resolve) => setTimeout(resolve, ms))
+const sleep = (ms = 300) => new Promise((resolve) => setTimeout(resolve, ms))
 
 /**
  * Updating logger with better UX.
